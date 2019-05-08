1158a09e3e0d4aa9c2b0f0cfc488609cf8036be2
Brandi Brandi <brandi.nicole22@gmail.com>
2019-05-08 16:49:02 -0700

    updated resumeData

---
+++ resume/src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+    "imagebaseurl":"https://techonoeticsclub.github.io/",
+    "name": "Brandi Chavez",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/brandii347",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/brandii347",
+          "className":"fa fa-github"
+        }
+        
+      ],
+    "aboutme":"I have finished my B.E. in Computer Science and have been working in industry for past few years.",
+    "address":"India",
+    "website":"https://www.codeclassifiers.com",
+    "education":[
+      {
+        "UniversityName":"Southern New Hampshire University",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"University of California, Irvine",
+        "specialization":"Coding Bootcamp",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Global Girls Give",
+        "specialization":"Web Developer",
+        "MonthOfLeaving":"November 2018",
+        "YearOfLeaving":"Present",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Golden Road Brewery",
+        "specialization":"Bartender",
+        "MonthOfLeaving":"November 2017",
+        "YearOfLeaving":"Present",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Oggi's Pizza and Brewing Co.",
+        "specialization":"Manager/Bartender",
+        "MonthOfLeaving":"December 2008",
+        "YearOfLeaving":"June 2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
