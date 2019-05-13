1d5bb82ae66e360c0f5f5014b224edbfc593cf22
Brandi Brandi <brandi.nicole22@gmail.com>
2019-05-13 15:32:22 -0700

    bug fixes

--- resume/src/resumeData.js
+++ resume/src/resumeData.js
@@ -1,13 +1,13 @@
 let resumeData = {
     "imagebaseurl":"https://techonoeticsclub.github.io/",
     "name": "Brandi Chavez",
-    "role": "Frontend Developer",
-    "linkedinId":"Your LinkedIn Id",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "role": "FullStack Developer",
+    "linkedinId":"brandii347",
+    "roleDescription": "I love learning new technologies. I do both frontend and backend development. I'm a mom of two and I love to bake in my free time. Also I'm a huge Star Wars nerd!",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/brandii347",
+          "url":"www.linkedin.com/in/brandii347",
           "className":"fa fa-linkedin"
         },
         {
@@ -17,46 +17,47 @@ let resumeData = {
         }
         
       ],
-    "aboutme":"I have finished my B.E. in Computer Science and have been working in industry for past few years.",
-    "address":"India",
-    "website":"https://www.codeclassifiers.com",
+    "aboutme":"I finished my coding Bootcamp back in April 2018, I have been learning more on my own and working for Global Girls Give as a Web developer since November 2018.",
+    "address":"Tustin, CA",
+    "email":"brandi.nicole22@gmail.com",
+    "website":"",
     "education":[
       {
         "UniversityName":"Southern New Hampshire University",
         "specialization":"Computer Science",
         "MonthOfPassing":"Jan",
         "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "Achievements":"Bachelors of Science"
       },
       {
         "UniversityName":"University of California, Irvine",
         "specialization":"Coding Bootcamp",
         "MonthOfPassing":"April",
         "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "Achievements":"Certificate"
       }
     ],
     "work":[
       {
         "CompanyName":"Global Girls Give",
         "specialization":"Web Developer",
-        "MonthOfLeaving":"November 2018",
+        "MonthOfLeaving":"November 2018-",
         "YearOfLeaving":"Present",
-        "Achievements":"Some Achievements"
+        "Achievements":""
       },
       {
         "CompanyName":"Golden Road Brewery",
         "specialization":"Bartender",
-        "MonthOfLeaving":"November 2017",
+        "MonthOfLeaving":"November 2017-",
         "YearOfLeaving":"Present",
-        "Achievements":"Some Achievements"
+        "Achievements":""
       },
       {
         "CompanyName":"Oggi's Pizza and Brewing Co.",
         "specialization":"Manager/Bartender",
-        "MonthOfLeaving":"December 2008",
+        "MonthOfLeaving":"December 2008-",
         "YearOfLeaving":"June 2018",
-        "Achievements":"Some Achievements"
+        "Achievements":""
       }
     ],
     "skillsDescription":"Your skills here",
@@ -65,22 +66,31 @@ let resumeData = {
         "skillname":"HTML5"
       },
       {
-        "skillname":"CSS"
+        "skillname":"Javascript"
       },
       {
         "skillname":"Reactjs"
-      }
+      },
+      {
+        "skillname":"MySQL"
+      },
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"MongoDB"
+      },
     ],
     "portfolio":[
       {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"Star Wars Trivia Game",
+        "description":"Just a Simple Star Wars Trivia Game",
+        "imgurl":"images/portfolio/trivia.png"
       },
       {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
+        "name":"Crystal Lightsaber Game",
+        "description":"Try and collect crystals to get to the correct number",
+        "imgurl":"images/portfolio/crystalgame.png"
       },
       {
         "name":"project3",
@@ -93,16 +103,7 @@ let resumeData = {
         "imgurl":"images/portfolio/phone.jpg"
       }
     ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
+  
   }
   
   export default resumeData
